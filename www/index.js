3d481e2e89c6297f252ecd721d59604a4dfb11a1
KoyamaSohei <koyamaso0309@gmail.com>
2019-06-12 23:18:49 +0900

    add control panel

--- www/index.js
+++ www/index.js
@@ -2,6 +2,7 @@ import { World, Cell } from "mandelbrot";
 import { memory } from 'mandelbrot/mandelbrot_bg';
 import { dx, dy, width, height, scale } from './param';
 import { drawCells } from './draw';
+import './panel';
 
 const world = World.new(width,height);
 
---
+++ www/panel.js
@@ -0,0 +1,40 @@
+import { width, height, dx, dy, scale } from './param';
+
+const plus = document.querySelector('button.plus');
+const minus = document.querySelector('button.minus');
+const up = document.querySelector('button.up');
+const down = document.querySelector('button.down');
+const left = document.querySelector('button.left');
+const right = document.querySelector('button.right');
+
+console.log(plus);
+
+plus.addEventListener('click',() => {
+  reload(width,height,dx,dy,scale*2);
+});
+minus.addEventListener('click',() => {
+  reload(width,height,dx,dy,scale/2);
+});
+up.addEventListener('click',() => {
+  reload(width,height,dx,dy+1,scale);
+});
+down.addEventListener('click',() => {
+  reload(width,height,dx,dy-1,scale);
+});
+left.addEventListener('click',() => {
+  reload(width,height,dx-1,dy,scale);
+});
+right.addEventListener('click',() => {
+  reload(width,height,dx+1,dy,scale);
+});
+
+
+const reload = (w,h,x,y,s) => {
+  const p = new URLSearchParams();
+  p.set('width',w);
+  p.set('height',h);
+  p.set('dx',x);
+  p.set('dy',y);
+  p.set('scale',s);
+  window.location = `${window.origin}/?${p.toString()}`;
+}
